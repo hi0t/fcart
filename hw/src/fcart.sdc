b9d591072638fbfa024b3241ca435737aca72141
hi0t <27971344+hi0t@users.noreply.github.com>
2025-10-01 18:50:50 +0300

    add summary during synthesis

--- hw/src/fcart.sdc
+++ hw/src/fcart.sdc
@@ -7,7 +7,9 @@ create_clock -name PPU_RD -period 500 [get_ports PPU_RD]
 
 set_clock_groups -exclusive \
     -group [get_clocks CLK] \
-    -group [get_clocks SDRAM_CLK] \
-    -group [get_clocks QSPI_CLK]
+    -group [get_clocks QSPI_CLK] \
+    -group [get_clocks M2] \
+    -group [get_clocks PPU_RD]
 
 set_false_path -from [get_ports QSPI_NCS]
+set_false_path -to [get_ports FPGA_IRQ]
---
+++ hw/summary.py
@@ -0,0 +1,98 @@
+import sys
+import re
+
+
+class bcolors:
+    HEADER = "\033[95m"
+    WARNING = "\033[93m"
+    ENDC = "\033[0m"
+
+
+if __name__ == "__main__":
+    log_file = sys.argv[1]
+
+    with open(log_file, "r") as f:
+        lines = f.readlines()
+
+    usage = []
+    for line in lines:
+        line = line.strip()
+        if line.startswith("Number of LUT4s:") or line.startswith(
+            "Number of registers:"
+        ):
+            usage.append(line)
+
+    print(bcolors.HEADER)
+    print("Usage summary")
+    print("*************")
+    print(bcolors.ENDC, end="")
+    for u in usage:
+        print(f"  {u}")
+
+    twr_file = sys.argv[2]
+    timings = []
+    curr_parts = {}
+    state = 0
+    skip = 4
+    with open(twr_file, "r") as f:
+        for line in f:
+            if state == 0:
+                if "Report Summary" in line:
+                    state = 1
+                continue
+
+            if state == 1:
+                if skip > 0:
+                    skip -= 1
+                    continue
+                else:
+                    state = 2
+
+            if state == 2:
+                if line.strip().startswith("---"):
+                    break
+
+                if "|" in line:
+                    parts = line.split("|")
+
+                    if not parts[0].strip():
+                        if curr_parts:
+                            timings.append(curr_parts)
+                            curr_parts = {}
+                        continue
+
+                    if curr_parts:
+                        curr_parts["preference"] += parts[0].strip()
+                        curr_parts["constraint"] += parts[1].strip()
+                        curr_parts["actual"] += parts[2].strip()
+                        curr_parts["levels"] += parts[3].strip()
+                    else:
+                        curr_parts = {
+                            "preference": parts[0].strip(),
+                            "constraint": parts[1].strip(),
+                            "actual": parts[2].strip(),
+                            "levels": parts[3].strip(),
+                        }
+
+    print(bcolors.HEADER)
+    print("Timing summary")
+    print("**************")
+    print(bcolors.ENDC, end="")
+
+    column_widths = [max(len(str(item)) + 4 for item in col) for col in zip(*timings)]
+
+    for i, t in enumerate(timings):
+        if t["constraint"] == "-":
+            continue
+
+        if t["levels"].endswith("*"):
+            print(f"{bcolors.WARNING}", end="")
+
+        preference = re.findall(r'"([^"]*)"', t["preference"])
+        print(
+            f"  {(preference[0]+":").ljust(column_widths[0])}{t['constraint'].ljust(column_widths[1])}{t['actual'].ljust(column_widths[2])}",
+            end="",
+        )
+        print(f"{bcolors.ENDC}")
+
+    print()
